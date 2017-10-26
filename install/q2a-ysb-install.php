510ff81321a6713a92a792d0dc89403757de26e4
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-10-26 15:38:06 +0900

    テーブル構成変更

--- install/q2a-ysb-install.php
+++ install/q2a-ysb-install.php
@@ -1,37 +1,40 @@
 <?php
 // don't allow this page to be requested directly from browser
 if (!defined('QA_VERSION')) {
-	header('Location: ../../');
-	exit;
+    header('Location: ../../');
+    exit;
 }
 
-class q2a_ysb_install {
+class q2a_ysb_install
+{
 
-  // 生成するテーブル。qa_ysb_のプレフィックスは抜き
-	private $table_names = array('actions', 'badges', 'badge_master');
+    // 生成するテーブル。qa_ysb_のプレフィックスは抜き
+    private $table_names = array('badges', 'badge_master');
 
-	/**
-	 * create table function of framework
-	 * @param  [type] $tableslc []
-	 * @return array  array of table creattion sqls
-	 */
-	function init_queries($tableslc) {
-		$queries = array();
+    /**
+     * create table function of framework
+     * @param  [type] $tableslc []
+     * @return array  array of table creattion sqls
+     */
+    function init_queries($tableslc)
+    {
+        $queries = array();
 
-    foreach($this->table_names as $table_name) {
-      if (!in_array(qa_db_add_table_prefix('ysb_' . $table_name), $tableslc)) {
-				$queries[] = file_get_contents(YSB_DIR . '/sql/qa_ysb_create_' . $table_name . '_table.sql');
-			}
+        foreach($this->table_names as $table_name) {
+            if (!in_array(qa_db_add_table_prefix('ysb_' . $table_name), $tableslc)) {
+                    $queries[] = file_get_contents(YSB_DIR . '/sql/qa_ysb_create_' . $table_name . '_table.sql');
+                }
+        }
+
+        if (count($queries) > 0) {
+            return $queries;
+        }
+
+        return null;
     }
 
-		if (count($queries) > 0) {
-			return $queries;
+    function process_event($event, $post_userid, $post_handle, $cookieid, $params)
+    {
+        // do nothing
     }
-
-    return null;
-	}
-
-	function process_event($event, $post_userid, $post_handle, $cookieid, $params) {
-    // do nothing
-  }
 }
--- sql/qa_ysb_create_badges_table.sql
+++ sql/qa_ysb_create_badges_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `^ysb_badges` (
   `badgeid` int(20) unsigned NOT NULL,
   `userid` int(10) unsigned NOT NULL,
-  `level` smallint unsigned NOT NULL,
+  `show_flag` smallint unsigned NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
