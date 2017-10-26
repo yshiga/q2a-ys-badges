a9e83f8ee6173849f0588567b38d22557aca3ea1
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-10-26 15:47:08 +0900

    使用しないものを削除

--- layer/q2a-ysb-layer.php
+++ layer/q2a-ysb-layer.php
@@ -7,34 +7,8 @@ class qa_html_theme_layer extends qa_html_theme_base
         $html = file_get_contents(YSB_DIR . '/html/badges.html');
         $this->output($html);
 
-        // foreach($this->content['custom']['badge_info'] as $badge){
-        //     $this->output('・');
-        //     $this->output(qa_lang('ysb/badge_name_' . $badge['badgeid']));
-        //     $this->output('level: ' . $badge['level'] .  '/' . MAX_BADGE_LEVEL);
-        //     if($badge['level'] < MAX_BADGE_LEVEL) {
-        //     $this->output('次のバッチまで: ' . $badge['need_action_count'] . '回');
-        //     }
-        //     $this->output('<br>');
-        // }
-        $html = '';
-
     } else {
         parent::main();
     }
   }
 }
-
-/*
-array(5) {
-  ["badgeid"]=>
-  string(5) "10001"
-  ["level"]=>
-  string(1) "2"
-  ["next_action_count"]=>
-  string(2) "10"
-  ["need_action_count"]=>
-  int(2)
-  ["current_action_count"]=>
-  string(1) "8"
-}
-*/
--- page/q2a-badge-page.php
+++ page/q2a-badge-page.php
@@ -39,48 +39,48 @@ class qa_badge_page
 
         $userid = qa_get_logged_in_userid();
 
-        $badges_model = new qa_ysb_badges();
-        $badges_owns = $badges_model->find_by_userid($userid);
+        // $badges_model = new qa_ysb_badges();
+        // $badges_owns = $badges_model->find_by_userid($userid);
 
-        $tmp = array();
-        foreach($badges_owns as $badge) {
-          $tmp[$badge['badgeid']] = $badge;
-        }
-        $badges_owns = $tmp;
+        // $tmp = array();
+        // foreach($badges_owns as $badge) {
+        //   $tmp[$badge['badgeid']] = $badge;
+        // }
+        // $badges_owns = $tmp;
 
-        $badge_info = array();
-        $badge_masters = qa_ysb_badge_master::find_all();
-        // 所有バッチの情報を生成する
-        foreach($badge_masters as $badge_master) {
-          $level = 0;
-          $badgeid =  $badge_master['badgeid'];
+        // $badge_info = array();
+        // $badge_masters = qa_ysb_badge_master::find_all();
+        // // 所有バッチの情報を生成する
+        // foreach($badge_masters as $badge_master) {
+        //   $level = 0;
+        //   $badgeid =  $badge_master['badgeid'];
 
-          if(!empty($badges_owns[$badgeid])) {
-            $level = $badges_owns[$badgeid]['level'];
-          }
+        //   if(!empty($badges_owns[$badgeid])) {
+        //     $level = $badges_owns[$badgeid]['level'];
+        //   }
 
-          $next_count = -1;
-          if($level < MAX_BADGE_LEVEL) {
-            $next_count = $badge_master['action_level_' . ($level+1)];
-          }
+        //   $next_count = -1;
+        //   if($level < MAX_BADGE_LEVEL) {
+        //     $next_count = $badge_master['action_level_' . ($level+1)];
+        //   }
 
-          $need_action_count = -1;
-          if($level == MAX_BADGE_LEVEL)  {
-            $need_action_count = 0;
-          } else {
-            $need_action_count = $next_count - $badges_owns[$badgeid]['count'];
-          }
+        //   $need_action_count = -1;
+        //   if($level == MAX_BADGE_LEVEL)  {
+        //     $need_action_count = 0;
+        //   } else {
+        //     $need_action_count = $next_count - $badges_owns[$badgeid]['count'];
+        //   }
 
-          $badge_info[] = array(
-            'badgeid' => $badge_master['badgeid'],
-            'level' => $level,
-            'next_action_count' => $next_count,
-            'need_action_count' => $need_action_count,
-            'current_action_count' => $badges_owns[$badgeid]['count']
-          );
+        //   $badge_info[] = array(
+        //     'badgeid' => $badge_master['badgeid'],
+        //     'level' => $level,
+        //     'next_action_count' => $next_count,
+        //     'need_action_count' => $need_action_count,
+        //     'current_action_count' => $badges_owns[$badgeid]['count']
+        //   );
 
-        }
-        $qa_content['custom']['badge_info'] = $badge_info;
+        // }
+        // $qa_content['custom']['badge_info'] = $badge_info;
 
         return $qa_content;
     }
