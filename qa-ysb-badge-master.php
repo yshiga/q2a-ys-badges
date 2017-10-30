e958d072db8c2185dea08a599d8360360d873397
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-10-30 11:46:27 +0900

    badge masterのデータを別ファイルに

--- qa-ysb-badge-master.php
+++ qa-ysb-badge-master.php
@@ -11,68 +11,7 @@ class qa_ysb_badge_master
 
     public function renew()
     {
-        $badges = array(
-            array(
-                'badgeid' => 101,
-                'name' => 'good_answer',
-                'event' => 'a_vote_up'
-            ),
-            array(
-                'badgeid' => 102,
-                'name' => 'answer',
-                'event' => 'a_post'
-            ),
-            array(
-                'badgeid' => 103,
-                'name' => 'quick_answer',
-                'event' => 'a_post'
-            ),
-            array(
-                'badgeid' => 104,
-                'name' => 'savior',
-                'event' => 'a_post'
-            ),
-            array(
-                'badgeid' => 105,
-                'name' => 'detail_answer',
-                'event' => 'a_post'
-            ),
-            array(
-                'badgeid' => 106,
-                'name' => 'answer_with_image',
-                'event' => 'a_post'
-            ),
-            array(
-                'badgeid' => 201,
-                'name' => 'questioner',
-                'event' => 'q_post'
-            ),
-            array(
-                'badgeid' => 202,
-                'name' => 'good_question',
-                'event' => 'q_vote_up'
-            ),
-            array(
-                'badgeid' => 203,
-                'name' => 'with_many_answer',
-                'event' => 'a_post'
-            ),
-            array(
-                'badgeid' => 204,
-                'name' => 'detail_question',
-                'event' => 'q_post'
-            ),
-            array(
-                'badgeid' => 205,
-                'name' => 'question_with_image',
-                'event' => 'q_post'
-            ),
-            array(
-                'badgeid' => 206,
-                'name' => 'question_with_video',
-                'event' => 'q_post'
-            )
-        );
+        $badges = include( YSB_DIR . '/qa-ysb-badge-master-data.php');
 
         qa_db_query_sub('DELETE FROM ' . self::TABLE_NAME);
         $sql = 'INSERT INTO ' . self::TABLE_NAME;
