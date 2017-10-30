1e207850a96da38988b78363cd1c071775851aaf
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-10-30 09:26:46 +0900

    scriptにメッセージ追加

--- script/qa-ysb-init-badge-master.php
+++ script/qa-ysb-init-badge-master.php
@@ -5,5 +5,13 @@ if (!defined('QA_VERSION')) {
 
 require_once YSB_DIR . '/qa-ysb-badge-master.php';
 
+echo 'badge master init start...'.PHP_EOL;
+try {
 $tmp = new qa_ysb_badge_master();
 $tmp->renew();
+} catch (Exception $e) {
+	echo 'An error occurred.'.PHP_EOL;
+	echo $e->getMessage();
+	exit;
+}
+echo 'badge master init done!'.PHP_EOL;
