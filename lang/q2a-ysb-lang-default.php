509638b5d3b5cbdb2482ca368cc96addfe893264
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-10-31 11:29:23 +0900

    回答バッジの言語ファイル対応

--- lang/q2a-ysb-lang-default.php
+++ lang/q2a-ysb-lang-default.php
@@ -1,11 +1,21 @@
 <?php
 return array(
-  'page_title'=>'バッチ',
+  'page_title'       =>'バッジ一覧',
+  'section_answer'   => '回答バッジ',
+  'section_question' => '質問バッジ',
+  'section_blog'     => '飼育日誌バッジ',
 
-  'badge_name_10001' => '人気の回答',
-  'badge_name_10002' => '回答者',
-  'badge_name_10003' => '即答',
-  'badge_name_10004' => '救世主',
-  'badge_name_10005' => '親切な回答',
+  'badge_head_101' => '良い回答',
+  'badge_body_101' => 'いいね！が３つ以上つく',
+  'badge_head_102' => '回答者',
+  'badge_body_102' => '回答を投稿する',
+  'badge_head_103' => '即答者',
+  'badge_body_103' => '質問投稿の15分以内に<br class="mdl-cell--hide-phone">回答する',
+  'badge_head_104' => '救世主',
+  'badge_body_104' => '24時間以上回答がない<br class="mdl-cell--hide-phone">質問に回答する',
+  'badge_head_105' => '詳しい回答',
+  'badge_body_105' => '500文字以上の回答を<br class="mdl-cell--hide-phone">投稿する',
+  'badge_head_106' => '画像つき回答',
+  'badge_body_106' => '画像つき回答を<br class="mdl-cell--hide-phone">投稿する',
 
 );
