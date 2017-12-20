7262506cbbeddd11c085b1ed68025b4d48665e8f
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-12-20 16:36:10 +0900

    過去の実績から好奇心旺盛バッジ

--- awards/qa-ysb-awards-question.php
+++ awards/qa-ysb-awards-question.php
@@ -298,6 +298,20 @@ class qa_ysb_awards_full_of_curiosity extends qa_ysb_awards_question_base
         }
 
     }
+
+    public function get_target_users_from_achievement($exclude)
+    {
+        $sql = 'SELECT userid';
+        $sql.= ' FROM ^posts';
+        $sql.= " WHERE type = 'Q'";
+        $sql.= " AND userid IS NOT NULL";
+        if (!empty($exclude)) {
+            $sql.= qa_db_apply_sub(' AND userid NOT IN (#)', array($exclude));
+        }
+        $sql.= " GROUP BY userid";
+        $sql.= " HAVING COUNT(userid) >= #";
+        return qa_db_read_all_values(qa_db_query_sub($sql, self::QUESTION_COUNT));
+    }
 }
 
 /*
