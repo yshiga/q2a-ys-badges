f38891756db5f655f59d79e12b9b878da0104521
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-12-20 15:24:17 +0900

    過去の実績から詳しい回答バッジ

--- awards/qa-ysb-awards-answer.php
+++ awards/qa-ysb-awards-answer.php
@@ -195,6 +195,34 @@ class qa_ysb_awards_detail_answer extends qa_ysb_awards_answer_base
             return false;
         }
     }
+
+    public function get_target_users_from_achievement($exclude)
+    {
+        $sql = 'SELECT a.userid, a.content';
+        $sql.= ' FROM ^posts a';
+        $sql.= ' INNER JOIN';
+        $sql.= ' (SELECT userid, MAX(CHAR_LENGTH(content)) AS clen';
+        $sql.= '   FROM ^posts';
+        $sql.= '   WHERE userid IS NOT NULL';
+        if (!empty($exclude)) {
+            $sql.= qa_db_apply_sub(' AND userid NOT IN (#)', array($exclude));
+        }
+        $sql.= "   AND type = 'A'";
+        $sql.= '   AND CHAR_LENGTH(content) >= #';
+        $sql.= '   GROUP BY userid) b';
+        $sql.= ' ON a.userid = b.userid';
+        $sql.= ' AND CHAR_LENGTH(a.content) = b.clen';
+        $posts = qa_db_read_all_assoc(qa_db_query_sub($sql, self::CHAR_LENGTH_THRESHOLD));
+
+        $users = array();
+        foreach ($posts as $post) {
+            if (mb_strlen(strip_tags($post['content']), "UTF-8") > self::CHAR_LENGTH_THRESHOLD) {
+                $users[] = $post['userid'];
+            }
+        }
+        return $users;
+    }
+
 }
 
 /*
