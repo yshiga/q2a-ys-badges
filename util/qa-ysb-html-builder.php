00762977b035008a893912f40554043b324032f4
yaimapp <gmonday730-mbp@yahoo.co.jp>
2018-01-04 15:21:49 +0900

    ランキングバッジの表示修正

--- util/qa-ysb-html-builder.php
+++ util/qa-ysb-html-builder.php
@@ -89,12 +89,14 @@ class qa_ysb_html_builder {
     {
         $html = '';
         if (!empty($ranking)) {
+            _log($ranking);
             $tmpl = file_get_contents(YSB_DIR . '/html/user-badge-ranking.html');
             $item_tmpl = file_get_contents(YSB_DIR.'/html/user-badge-item.html');
             $list = '';
-            foreach($ranking as $id => $date) {
+            foreach($ranking as $badge) {
+                $id = $badge['badgeid'];
                 $image_url=qa_opt('site_url').self::IMAGE_BASE.'badge_'.$id.'.svg';
-                $dates = explode('-', $date);
+                $dates = explode('-', $badge['award_date']);
                 $param = array(
                     '^year' => $dates[0],
                     '^month' => $dates[1]
